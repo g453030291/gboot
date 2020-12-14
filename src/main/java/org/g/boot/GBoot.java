7322989ea9fe03a379d02d209c991c00eaf6da0d
gemushen <453030291@qq.com>
2020-12-14 23:21:48 +0800

    基于普通new线程，实现解析http get请求

--- src/main/java/org/g/boot/GBoot.java
+++ src/main/java/org/g/boot/GBoot.java
@@ -1,10 +1,8 @@
 package org.g.boot;
 
 import org.g.tcp.HttpHandle;
-import org.g.tcp.HttpMethod;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -22,25 +20,10 @@ public class GBoot {
 	 */
 	public static void main(String[] args) throws IOException {
 		ServerSocket serverSocket = new ServerSocket(8888,2);
-		Socket socket = serverSocket.accept();
-		byte[] bytes = new byte[1024];
-		StringBuilder sb = new StringBuilder();
-		InputStream inputStream = socket.getInputStream();
-		int len = 0;
-		while ((len = inputStream.read(bytes)) != -1){
-			sb.append(new String(bytes,0,len,"UTF-8"));
-			System.out.println(sb.toString());
+		while (true){
+			Socket socket = serverSocket.accept();
+			new Thread(new HttpHandle(socket)).start();
 		}
-		String[] split = sb.toString().split("\r\n");
-		if(split[0].startsWith(HttpMethod.GET.toString())){
-			HttpHandle.doGet(split);
-		}
-
-		System.out.println(split.length);
-		System.out.println(split[split.length-1]);
-		inputStream.close();
-		socket.close();
-		serverSocket.close();
 	}
 
 }
--- src/main/java/org/g/tcp/HttpHandle.java
+++ src/main/java/org/g/tcp/HttpHandle.java
@@ -1,13 +1,52 @@
 package org.g.tcp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
 /**
  * @author g
  * @date 2020/12/13 5:33 下午
  */
-public class HttpHandle {
+public class HttpHandle implements Runnable {
 
-	public static void doGet(String[] data){
+	private Socket socket;
 
+	public HttpHandle(Socket socket) {
+		this.socket = socket;
+	}
+
+	@Override
+	public void run() {
+		byte[] bytes = new byte[1024];
+		StringBuilder sb = new StringBuilder();
+		InputStream inputStream = null;
+		try {
+			inputStream = socket.getInputStream();
+			int len = 0;
+			while ((len = inputStream.read(bytes)) != -1){
+				sb.append(new String(bytes,0,len,"UTF-8"));
+				System.out.println(sb.toString());
+			}
+			String[] split = sb.toString().split("\r\n");
+			if(split[0].startsWith(HttpMethod.GET.toString())){
+				doGet(split);
+			}
+			inputStream.close();
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 执行get请求
+	 * @param split
+	 */
+	private void doGet(String[] split){
+		for (int i = 0; i < split.length; i++) {
+			System.out.println(split[i]);
+		}
 	}
 
 }
