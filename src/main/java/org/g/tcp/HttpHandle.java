f9103f3148f1b6bc5a87be50832fa13d7a9ea352
gemushen <453030291@qq.com>
2020-12-16 23:15:25 +0800

    修改get方法解析数据，后续需要重新抽象数据流读取的方法

--- src/main/java/org/g/tcp/HttpHandle.java
+++ src/main/java/org/g/tcp/HttpHandle.java
@@ -3,6 +3,8 @@ package org.g.tcp;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author g
@@ -43,10 +45,33 @@ public class HttpHandle implements Runnable {
 	 * 执行get请求
 	 * @param split
 	 */
-	private void doGet(String[] split){
+	private Map<String,String> doGet(String[] split){
+//		GET /?aa=99 HTTP/1.1
+//		Host: localhost:8888
+//		Connection: keep-alive
+//		sec-ch-ua: "Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"
+//		sec-ch-ua-mobile: ?0
+//		Upgrade-Insecure-Requests: 1
+//		User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36
+//		Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+//Sec-Fetch-Site: none
+//Sec-Fetch-Mode: navigate
+//Sec-Fetch-User: ?1
+//Sec-Fetch-Dest: document
+//Accept-Encoding: gzip, deflate, br
+//Accept-Language: zh-CN,zh;q=0.9,en;q=0.8
+//Cookie: Webstorm-47bc3066=5b3d46d2-5415-40ef-9c18-410f6d0fd04c
+		Map<String,String> map = new HashMap<String,String>();
 		for (int i = 0; i < split.length; i++) {
-			System.out.println(split[i]);
+			String param = split[0].split(" ")[1];
+			String param2 = param.substring(2,param.length());
+			String [] param3 = param2.split("&");
+			for (int j = 0; j < param3.length; j++) {
+				String[] param4 = param3[j].split("=");
+				map.put(param4[0],param4[1]);
+			}
 		}
+		return  map;
 	}
 
 }
